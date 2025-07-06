287d30db1951e0031bb785955835f8c69c51aa81
shreyash vishwakarma <shreyasvishwakarma00@gmail.com>
2025-07-06 14:04:01 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@
 export const dynamic = 'force-dynamic';
 import { prisma } from '../lib/prisma';
 import ProductCard from '../components/ProductCard';
+export const dynamic = 'force-dynamic';
+
 
 export default async function Home() {
   const products = await prisma.Product.findMany();

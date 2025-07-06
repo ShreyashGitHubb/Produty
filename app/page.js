4b6d1a32a1cbcbe0b5a77e1f619788497cabfa94
shreyash vishwakarma <shreyasvishwakarma00@gmail.com>
2025-07-06 14:09:59 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
-// import { prisma } from '@/lib/prisma';
-export const dynamic = 'force-dynamic';
-import { prisma } from '../lib/prisma';
-import ProductCard from '../components/ProductCard';
 export const dynamic = 'force-dynamic';
 
+import { prisma } from '../lib/prisma';
+import ProductCard from '../components/ProductCard';
 
 export default async function Home() {
   const products = await prisma.Product.findMany();
+
   return (
     <div className="flex flex-wrap justify-center items-center gap-6 px-4 py-10 bg-[#ffffff] min-h-screen">
       {products.map((p) => (
